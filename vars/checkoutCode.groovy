7685b8fa3ce10fa381048a985ec41da92a0d14dd
npkdocs <npkdocs@gmail.com>
2024-06-12 11:19:48 +0530

    checkout scm fucntionality added

---
+++ vars/checkoutCode.groovy
@@ -0,0 +1,6 @@
+def checkout(String repoUrl, String branch) {
+    def workingDir = "${env.WORKSPACE}"
+    sh "git clone ${repoUrl} ${workingDir}"
+    sh "git checkout ${branch}"
+    return workingDir
+}
